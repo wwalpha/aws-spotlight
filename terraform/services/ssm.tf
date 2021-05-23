b390c024daaef84a1067707d71191997cba1cb55
wwalpha <wwalpha@gmail.com>
2021-05-23 12:51:59 +0000

    add resource manager envs

--- terraform/services/ssm.tf
+++ terraform/services/ssm.tf
@@ -15,3 +15,22 @@ resource "aws_ssm_parameter" "auth_envs" {
     ]
   }
 }
+
+# ----------------------------------------------------------------------------------------------
+# SSM Parameter Store - Environment variables
+# ----------------------------------------------------------------------------------------------
+resource "aws_ssm_parameter" "resource_envs" {
+  name = "/${local.project_name}/environment/resource_manager"
+  type = "String"
+  value = jsonencode({
+    ENV_VARS = "DUMMY"
+  })
+  overwrite = true
+
+  lifecycle {
+    ignore_changes = [
+      value
+    ]
+  }
+}
+
