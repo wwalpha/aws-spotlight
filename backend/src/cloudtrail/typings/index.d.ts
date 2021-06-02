98e5887cf05575cf72f99f0940c3e522a633d6ab
wwalpha <wwalpha@gmail.com>
2021-06-02 23:08:38 +0000

    bugfix

--- backend/src/cloudtrail/typings/index.d.ts
+++ backend/src/cloudtrail/typings/index.d.ts
@@ -1,6 +1,3 @@
-import * as Tables from '../../../typings/tables';
-import { CloudTrail } from '../../../typings/cloudtrail';
-
-export { Tables, CloudTrail };
+export * from '../../../typings/index';
 
 export type EVENT_TYPE = Record<string, Tables.EventType>;
