27d1f127fa07201f3d0f8343157049012c833d7a
wwalpha <wwalpha@gmail.com>
2021-06-02 23:19:20 +0000

    bugfix

--- backend/src/user_manager/jest.config.ts
+++ backend/src/user_manager/jest.config.ts
@@ -4,6 +4,8 @@ const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   setupFiles: ['dotenv/config'],
+  passWithNoTests: true,
+  forceExit: true,
 };
 
 export default config;
