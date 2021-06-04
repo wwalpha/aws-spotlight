5691c706e3e19ae3e8c00741322c1c849cd4a4ee
wwalpha <wwalpha@gmail.com>
2021-06-04 23:05:10 +0000

    fix typings

--- backend/typings/index.d.ts
+++ backend/typings/index.d.ts
@@ -1,11 +1,8 @@
 export * from './cloudtrail';
-
-import * as Resource from './resource';
+export * from './resource';
 export * from './users';
 export * from './token';
 export * from './tables';
 export * from './auth';
-
-export { Resource };
 
 export type EVENT_TYPE = Record<string, Tables.EventType>;
