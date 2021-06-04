d37a1a294861c52ccb85229d8ef936eecac336b0
wwalpha <wwalpha@gmail.com>
2021-06-04 09:56:11 +0000

    update api path

--- backend/terraform/apigateway.tf
+++ backend/terraform/apigateway.tf
@@ -1,43 +1,60 @@
 # ---------------------------------------------------------------------------------------------
-# API Gateway Route
+# API Gateway Route - GET /user/health
 # ---------------------------------------------------------------------------------------------
-resource "aws_apigatewayv2_route" "user_health" {
+resource "aws_apigatewayv2_route" "get_user_health" {
   api_id    = local.apigateway_id
   route_key = "GET /user/health"
   target    = "integrations/${local.apigateway_integration_user}"
 }
 
-# resource "aws_apigatewayv2_route" "http_post" {
-#   api_id             = local.api_gateway_id
-#   route_key          = "POST /{proxy+}"
-#   target             = "integrations/${local.api_gateway_integration_resource}"
-#   authorizer_id      = local.apigateway_authorizer_id
-#   authorization_type = "JWT"
-# }
+# ---------------------------------------------------------------------------------------------
+# API Gateway Route - POST /user
+# ---------------------------------------------------------------------------------------------
+resource "aws_apigatewayv2_route" "post_user" {
+  api_id    = local.apigateway_id
+  route_key = "POST /user"
+  target    = "integrations/${local.apigateway_integration_user}"
+  # authorizer_id      = local.apigateway_authorizer_id
+  # authorization_type = "JWT"
+}
 
-# resource "aws_apigatewayv2_route" "http_get" {
-#   api_id             = local.apigateway_id
-#   route_key          = "GET /{proxy+}"
-#   target             = "integrations/${aws_apigatewayv2_integration.resource.id}"
-#   authorizer_id      = local.apigateway_authorizer_id
-#   authorization_type = "JWT"
-# }
+# ---------------------------------------------------------------------------------------------
+# API Gateway Route - POST /user/admin
+# ---------------------------------------------------------------------------------------------
+resource "aws_apigatewayv2_route" "post_user_admin" {
+  api_id    = local.apigateway_id
+  route_key = "POST /user/admin"
+  target    = "integrations/${local.apigateway_integration_user}"
+  # authorizer_id      = local.apigateway_authorizer_id
+  # authorization_type = "JWT"
+}
 
-# resource "aws_apigatewayv2_route" "http_put" {
-#   api_id             = local.apigateway_id
-#   route_key          = "PUT /{proxy+}"
-#   target             = "integrations/${aws_apigatewayv2_integration.resource.id}"
-#   authorizer_id      = local.apigateway_authorizer_id
-#   authorization_type = "JWT"
-# }
+# ---------------------------------------------------------------------------------------------
+# API Gateway Route - GET /resource/health
+# ---------------------------------------------------------------------------------------------
+resource "aws_apigatewayv2_route" "get_resource_health" {
+  api_id    = local.apigateway_id
+  route_key = "GET /resource/health"
+  target    = "integrations/${local.apigateway_integration_resource}"
+}
 
-# resource "aws_apigatewayv2_route" "http_delete" {
-#   api_id             = local.apigateway_id
-#   route_key          = "DELETE /{proxy+}"
-#   target             = "integrations/${aws_apigatewayv2_integration.resource.id}"
-#   authorizer_id      = local.apigateway_authorizer_id
-#   authorization_type = "JWT"
-# }
+# ---------------------------------------------------------------------------------------------
+# API Gateway Route - GET /auth/health
+# ---------------------------------------------------------------------------------------------
+resource "aws_apigatewayv2_route" "get_auth_health" {
+  api_id    = local.apigateway_id
+  route_key = "GET /auth/health"
+  target    = "integrations/${local.apigateway_integration_auth}"
+}
+
+# ---------------------------------------------------------------------------------------------
+# API Gateway Route - POST /auth/signin
+# ---------------------------------------------------------------------------------------------
+resource "aws_apigatewayv2_route" "post_auth_signin" {
+  api_id    = local.apigateway_id
+  route_key = "POST /auth/signin"
+  target    = "integrations/${local.apigateway_integration_auth}"
+}
 
 # resource "aws_apigatewayv2_route" "http_options" {
 #   api_id    = local.apigateway_id
