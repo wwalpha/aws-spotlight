2231429a8fe892bfb727c2f5e2e5da60de29f0a5
wwalpha <wwalpha@gmail.com>
2021-06-05 14:25:00 +0000

    remove console.log in building

--- frontend/configs/webpack/webpack.prod.ts
+++ frontend/configs/webpack/webpack.prod.ts
@@ -2,6 +2,7 @@ import { Configuration, EnvironmentPlugin } from 'webpack';
 import merge from 'webpack-merge';
 import baseConfig from './webpack.base';
 import CompressionPlugin from 'compression-webpack-plugin';
+import TerserWebpackPlugin from 'terser-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import * as path from 'path';
 
@@ -21,6 +22,21 @@ const prod: Configuration = {
     //   filename: '[path].gz[query]',
     // }),
   ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserWebpackPlugin({
+        terserOptions: {
+          format: {
+            comments: false,
+          },
+          compress: {
+            drop_console: true,
+          },
+        },
+      }),
+    ],
+  },
 };
 
 export default merge(baseConfig, prod);
