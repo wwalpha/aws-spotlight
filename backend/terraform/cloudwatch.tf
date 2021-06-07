a613414a6623d2737a5128ee5574d25f6d49979b
wwalpha <wwalpha@gmail.com>
2021-06-07 11:00:14 +0000

    change cron settings

--- backend/terraform/cloudwatch.tf
+++ backend/terraform/cloudwatch.tf
@@ -4,7 +4,7 @@
 resource "aws_cloudwatch_event_rule" "unprocessed" {
   name                = "${local.project_name_uc}_UnprocessedRule"
   description         = "unprocessed schedule"
-  schedule_expression = "cron(0 0/12 * * ? *)"
+  schedule_expression = "cron(0 0/3 * * ? *)"
 }
 
 # ----------------------------------------------------------------------------------------------
