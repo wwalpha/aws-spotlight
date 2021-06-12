8606c5617992abb7dd8813544fa42dcf77a5ba26
wwalpha <wwalpha@gmail.com>
2021-06-12 10:14:24 +0000

    patch

--- terraform/services/sqs.tf
+++ terraform/services/sqs.tf
@@ -2,12 +2,13 @@
 # AWS SQS - CloudTrail
 # ----------------------------------------------------------------------------------------------
 resource "aws_sqs_queue" "cloudtrail" {
-  name_prefix                = "${local.project_name}-cloudtrail-"
-  delay_seconds              = 90
-  visibility_timeout_seconds = 300
-  max_message_size           = 262144
-  message_retention_seconds  = 604800
-  receive_wait_time_seconds  = 5
+  name_prefix                       = "${local.project_name}-cloudtrail-"
+  delay_seconds                     = 90
+  visibility_timeout_seconds        = 300
+  kms_data_key_reuse_period_seconds = 300
+  max_message_size                  = 262144
+  message_retention_seconds         = 604800
+  receive_wait_time_seconds         = 5
   redrive_policy = jsonencode({
     deadLetterTargetArn = aws_sqs_queue.deadletter.arn
     maxReceiveCount     = 10
@@ -44,10 +45,11 @@ resource "aws_sqs_queue_policy" "cloudtrail" {
 # AWS SQS - DeadLetter
 # ----------------------------------------------------------------------------------------------
 resource "aws_sqs_queue" "deadletter" {
-  name_prefix               = "${local.project_name}-deadletter-"
-  delay_seconds             = 90
-  max_message_size          = 2048
-  message_retention_seconds = 604800
-  receive_wait_time_seconds = 10
+  name_prefix                       = "${local.project_name}-deadletter-"
+  delay_seconds                     = 90
+  max_message_size                  = 2048
+  message_retention_seconds         = 604800
+  kms_data_key_reuse_period_seconds = 300
+  receive_wait_time_seconds         = 10
 }
 
