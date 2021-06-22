aec0d96f6ec563d8610058a6c9d0c185d289823b
wwalpha <wwalpha@gmail.com>
2021-06-22 22:56:18 +0000

    bugfix

--- backend/src/resource_manager/test/index.test.ts
+++ backend/src/resource_manager/test/index.test.ts
@@ -1,10 +1,30 @@
 import request from 'supertest';
+import { sign } from 'jsonwebtoken';
+import * as fs from 'fs';
 import server from '../src/server';
-import CATEGORIES_ADMIN from './datas/categories_admin.json';
-import CATEGORIES_USER from './datas/categories_user.json';
-import RESOURCE_ADMIN from './datas/resources_admin.json';
-import RESOURCE_USER from './datas/resources_user.json';
+import CATEGORIES_ADMIN from './expect/categories_admin.json';
+import CATEGORIES_USER from './expect/categories_user.json';
+import RESOURCE_ADMIN from './expect/resources_admin.json';
+import RESOURCE_USER from './expect/resources_user.json';
 import { Resource } from 'typings';
+
+const userIdToken = sign(
+  {
+    'custom:role': 'TENANT_USER',
+    'cognito:username': 'test001@gmail.com',
+  },
+  fs.readFileSync('./test/configs/private.key'),
+  { algorithm: 'RS256' }
+);
+
+const adminIdToken = sign(
+  {
+    'custom:role': 'TENANT_ADMIN',
+    'cognito:username': 'admin',
+  },
+  fs.readFileSync('./test/configs/private.key'),
+  { algorithm: 'RS256' }
+);
 
 describe('resource manager', () => {
   // Health Check
@@ -20,9 +40,7 @@ describe('resource manager', () => {
 
   // Get Service Resource List
   test('Get service resource list (admin)', async () => {
-    const response = await request(server)
-      .get('/resources/services/ec2')
-      .set({ Authorization: process.env.AUTU_TOKEN_ADMIN });
+    const response = await request(server).get('/resources/services/ec2').set({ Authorization: adminIdToken });
 
     expect(response.statusCode).toBe(200);
     expect(response.body).toEqual({
@@ -32,9 +50,16 @@ describe('resource manager', () => {
 
   // Get Service Resource List
   test('Get service resource list (user)', async () => {
-    const response = await request(server)
-      .get('/resources/services/ec2')
-      .set({ Authorization: process.env.AUTU_TOKEN_USER });
+    const userIdToken = sign(
+      {
+        'custom:role': 'TENANT_USER',
+        'cognito:username': 'test003@gmail.com',
+      },
+      fs.readFileSync('./test/configs/private.key'),
+      { algorithm: 'RS256' }
+    );
+
+    const response = await request(server).get('/resources/services/ec2').set({ Authorization: userIdToken });
 
     expect(response.statusCode).toBe(200);
     expect(response.body).toEqual({
@@ -44,9 +69,7 @@ describe('resource manager', () => {
 
   // Get Service Resource List
   test('Get category list (admin)', async () => {
-    const response = await request(server)
-      .get('/resources/categories')
-      .set({ Authorization: process.env.AUTU_TOKEN_ADMIN });
+    const response = await request(server).get('/resources/categories').set({ Authorization: adminIdToken });
 
     expect(response.statusCode).toBe(200);
     expect(response.body).toEqual(CATEGORIES_ADMIN);
@@ -54,9 +77,7 @@ describe('resource manager', () => {
 
   // Get Service Resource List
   test('Get category list (user)', async () => {
-    const response = await request(server)
-      .get('/resources/categories')
-      .set({ Authorization: process.env.AUTU_TOKEN_USER });
+    const response = await request(server).get('/resources/categories').set({ Authorization: userIdToken });
 
     expect(response.statusCode).toBe(200);
     expect(response.body).toEqual(CATEGORIES_USER);
