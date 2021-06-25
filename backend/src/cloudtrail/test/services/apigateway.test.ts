0f8e43089648f180e359516d184a0fc335c67cdc
wwalpha <wwalpha@gmail.com>
2021-06-25 09:45:02 +0000

    update test data

--- backend/src/cloudtrail/test/services/apigateway.test.ts
+++ backend/src/cloudtrail/test/services/apigateway.test.ts
@@ -1,17 +1,10 @@
 import AWS from 'aws-sdk';
 import { getHistory, getResource, sendMessage } from '@test/configs/utils';
-import APIGATEWAY_CreateRestApi from '../datas/create/APIGATEWAY_CreateRestApi.json';
-import APIGATEWAY_ImportRestApi from '../datas/create/APIGATEWAY_ImportRestApi.json';
-import APIGATEWAY_DeleteRestApi from '../datas/delete/APIGATEWAY_DeleteRestApi.json';
-
 import { cloudtrail } from '@src/index';
-import {
-  CreateRestApi_H,
-  CreateRestApi_R,
-  DeleteRestApi_H,
-  ImportRestApi_H,
-  ImportRestApi_R,
-} from '@test/expect/apigateway';
+import * as CreateEvents from '@test/datas/create';
+import * as DeleteEvents from '@test/datas/delete';
+import * as APIGATEWAY from '@test/expect/apigateway';
+import * as fs from 'fs';
 
 AWS.config.update({
   region: process.env.AWS_REGION,
@@ -22,46 +15,55 @@ AWS.config.update({
 
 describe('apigateway.amazonaws.com', () => {
   test('CreateRestApi', async () => {
-    const event = await sendMessage(APIGATEWAY_CreateRestApi);
+    const event = await sendMessage(CreateEvents.APIGATEWAY_CreateRestApi);
 
     await cloudtrail(event);
 
-    const resource = await getResource({ EventSource: 'apigateway.amazonaws.com', ResourceId: 'jrrfh5tt86' });
+    const resource = await getResource({
+      EventSource: 'apigateway.amazonaws.com',
+      ResourceId: 'arn:aws:apigateway:ap-northeast-1::/apis/jrrfh5tt86',
+    });
     const history = await getHistory({ EventId: '90baa69e-caed-44bb-85fe-004348b07ea0' });
 
     expect(resource).not.toBeUndefined();
-    expect(resource).toEqual(CreateRestApi_R);
+    expect(resource).toEqual(APIGATEWAY.CreateRestApi_R);
 
     expect(history).not.toBeUndefined();
-    expect(history).toEqual(CreateRestApi_H);
+    expect(history).toEqual(APIGATEWAY.CreateRestApi_H);
   });
 
   test('ImportRestApi', async () => {
-    const event = await sendMessage(APIGATEWAY_ImportRestApi);
+    const event = await sendMessage(CreateEvents.APIGATEWAY_ImportRestApi);
 
     await cloudtrail(event);
 
-    const resource = await getResource({ EventSource: 'apigateway.amazonaws.com', ResourceId: 'lyppg996u0' });
+    const resource = await getResource({
+      EventSource: 'apigateway.amazonaws.com',
+      ResourceId: 'arn:aws:apigateway:ap-northeast-1::/apis/lyppg996u0',
+    });
     const history = await getHistory({ EventId: '86a4e780-56fc-422a-b95d-9f19ca5e2c11' });
 
     expect(resource).not.toBeUndefined();
-    expect(resource).toEqual(ImportRestApi_R);
+    expect(resource).toEqual(APIGATEWAY.ImportRestApi_R);
 
     expect(history).not.toBeUndefined();
-    expect(history).toEqual(ImportRestApi_H);
+    expect(history).toEqual(APIGATEWAY.ImportRestApi_H);
   });
 
   test('DeleteRestApi', async () => {
-    const event = await sendMessage(APIGATEWAY_DeleteRestApi);
+    const event = await sendMessage(DeleteEvents.APIGATEWAY_DeleteRestApi);
 
     await cloudtrail(event);
 
-    const resource = await getResource({ EventSource: 'apigateway.amazonaws.com', ResourceId: 'jrrfh5tt86' });
+    const resource = await getResource({
+      EventSource: 'apigateway.amazonaws.com',
+      ResourceId: 'arn:aws:apigateway:ap-northeast-1::/apis/jrrfh5tt86',
+    });
     const history = await getHistory({ EventId: '86a4e780-56fc-422a-b95d-9f19ca5e2c11' });
 
     expect(resource).toBeUndefined();
 
     expect(history).not.toBeUndefined();
-    expect(history).toEqual(DeleteRestApi_H);
+    expect(history).toEqual(APIGATEWAY.DeleteRestApi_H);
   });
 });
