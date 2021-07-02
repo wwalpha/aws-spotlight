7e045547a3aa9836339018db777621eced27745a
wwalpha <wwalpha@gmail.com>
2021-07-02 12:03:27 +0000

    add release notes

--- backend/src/release_events/src/patch.ts
+++ backend/src/release_events/src/patch.ts
@@ -1,7 +1,24 @@
-import { reCreateFromBucket } from './utils';
+import { DynamodbHelper } from '@alphax/dynamodb';
+import { Environments } from './consts';
 
 const start = async () => {
-  await reCreateFromBucket();
+  const helper = new DynamodbHelper();
+
+  await helper.put({
+    TableName: Environments.TABLE_NAME_SETTINGS,
+    Item: {
+      Id: 'GLOBAL_SERVICES',
+      Services: ['iam.amazonaws.com'],
+    },
+  });
+
+  await helper.put({
+    TableName: Environments.TABLE_NAME_SETTINGS,
+    Item: {
+      Id: 'API_KEY',
+      Keys: ['9999999999'],
+    },
+  });
 };
 
 start();
