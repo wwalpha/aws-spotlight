5fbd17df90f7bb26da217f1706dd36517277ccd3
wwalpha <wwalpha@gmail.com>
2021-07-31 03:19:18 +0000

    bugfix

--- backend/src/cloudtrail/src/process/tags/EC2_Instance.ts
+++ backend/src/cloudtrail/src/process/tags/EC2_Instance.ts
@@ -2,13 +2,27 @@ import { EC2 } from 'aws-sdk';
 
 const client = new EC2();
 
-export const EC2_Instance = async (instanceId: string, owner: string) =>
-  client.createTags({
-    Resources: [instanceId],
-    Tags: [
-      {
-        Key: 'Owner',
-        Value: owner,
-      },
-    ],
-  });
+export const EC2_Instance = async (instanceId: string, owner: string) => {
+  // check resource exsit
+  const results = await client
+    .describeInstances({
+      InstanceIds: [instanceId],
+    })
+    .promise();
+
+  // validation
+  if (!results.Reservations || results.Reservations.length === 0) return;
+
+  // add tag
+  await client
+    .createTags({
+      Resources: [instanceId],
+      Tags: [
+        {
+          Key: 'Owner',
+          Value: owner,
+        },
+      ],
+    })
+    .promise();
+};
--- backend/src/cloudtrail/src/process/tags/RDS_DBInstance.ts
+++ backend/src/cloudtrail/src/process/tags/RDS_DBInstance.ts
@@ -2,13 +2,26 @@ import { RDS } from 'aws-sdk';
 
 const client = new RDS();
 
-export const RDS_DBInstance = async (arn: string, owner: string) =>
-  client.addTagsToResource({
-    ResourceName: arn,
-    Tags: [
-      {
-        Key: 'Owner',
-        Value: owner,
-      },
-    ],
-  });
+export const RDS_DBInstance = async (arn: string, owner: string) => {
+  // check resource exsit
+  const results = await client
+    .describeDBInstances({
+      DBInstanceIdentifier: arn.split('/')[1],
+    })
+    .promise();
+
+  // validation
+  if (!results.DBInstances || results.DBInstances.length === 0) return;
+
+  await client
+    .addTagsToResource({
+      ResourceName: arn,
+      Tags: [
+        {
+          Key: 'Owner',
+          Value: owner,
+        },
+      ],
+    })
+    .promise();
+};
