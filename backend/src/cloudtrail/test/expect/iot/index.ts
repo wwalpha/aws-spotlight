6f3a30ab9a26a53d11bf1e7e99d1f659274e4601
wwalpha <wwalpha@gmail.com>
2023-04-05 07:55:26 +0900

    update

---
+++ backend/src/cloudtrail/test/expect/iot/index.ts
@@ -0,0 +1,3 @@
+export { default as IOT_CreateTopicRule_H } from './IOT_CreateTopicRule_H.json';
+export { default as IOT_CreateTopicRule_R } from './IOT_CreateTopicRule_R.json';
+export { default as IOT_DeleteTopicRule_H } from './IOT_DeleteTopicRule_H.json';
