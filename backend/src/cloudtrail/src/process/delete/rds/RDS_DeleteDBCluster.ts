69c46b17f1e57a36489a4424a9a4bd0e543aa354
wwalpha <wwalpha@gmail.com>
2023-04-05 19:14:52 +0900

    bugfix

--- backend/src/cloudtrail/src/process/delete/rds/RDS_DeleteDBCluster.ts
+++
@@ -1,6 +0,0 @@
-import { CloudTrail, Tables } from 'typings';
-
-export const RDS_DeleteDBCluster = (record: CloudTrail.Record): Tables.ResouceGSI1Key => ({
-  EventSource: record.eventSource,
-  ResourceId: record.responseElements.dBClusterArn,
-});
--- backend/src/cloudtrail/src/process/delete/rds/RDS_DeleteDBProxy.ts
+++
@@ -1,6 +0,0 @@
-import { CloudTrail, Tables } from 'typings';
-
-export const RDS_DeleteDBProxy = (record: CloudTrail.Record): Tables.ResouceGSI1Key => ({
-  EventSource: record.eventSource,
-  ResourceId: record.responseElements.dBProxy.dBProxyArn,
-});
