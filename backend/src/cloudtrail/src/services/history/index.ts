ba93e73ea6dda751db3df007b26cd628bf94959f
wwalpha <wwalpha@gmail.com>
2023-06-08 09:05:43 +0000

    update

--- backend/src/cloudtrail/src/services/history/index.ts
+++ backend/src/cloudtrail/src/services/history/index.ts
@@ -3,19 +3,19 @@ import { Tables } from 'typings';
 import * as Queries from './queries';
 
 /** 詳細取得 */
-export const describe = async (key: Tables.TIgnoreKey): Promise<Tables.TIgnore | undefined> => {
-  const results = await DynamodbHelper.get<Tables.TIgnore>(Queries.get(key));
+export const describe = async (key: Tables.THistoryKey): Promise<Tables.THistory | undefined> => {
+  const results = await DynamodbHelper.get<Tables.THistory>(Queries.get(key));
 
   return results?.Item;
 };
 
 /** 内容更新 */
-export const regist = async (item: Tables.TIgnore): Promise<void> => {
+export const regist = async (item: Tables.THistory): Promise<void> => {
   await DynamodbHelper.put(Queries.put(item));
 };
 
 /** 内容更新 */
-export const update = async (item: Tables.TIgnore): Promise<void> => {
+export const update = async (item: Tables.THistory): Promise<void> => {
   const groupInfo = await describe({
     EventId: item.EventId,
   });
@@ -29,13 +29,13 @@ export const update = async (item: Tables.TIgnore): Promise<void> => {
 };
 
 /** グループ削除 */
-export const remove = async (key: Tables.TIgnoreKey): Promise<void> => {
+export const remove = async (key: Tables.THistoryKey): Promise<void> => {
   await DynamodbHelper.delete(Queries.del(key));
 };
 
 /** イベント一覧取得 */
-export const listByEventName = async (eventSource: string, eventName: string): Promise<Tables.TIgnore[]> => {
-  const res = await DynamodbHelper.query<Tables.TIgnore>(Queries.listByEventName(eventSource, eventName));
+export const listByEventName = async (eventSource: string, eventName: string): Promise<Tables.THistory[]> => {
+  const res = await DynamodbHelper.query<Tables.THistory>(Queries.listByEventName(eventSource, eventName));
 
   return res.Items || [];
 };
