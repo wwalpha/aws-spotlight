6d083b9ac96929b40922b6370e56b16baf50bf17
wwalpha <wwalpha@gmail.com>
2023-06-08 03:49:06 +0000

    update

--- terraform/services/sqs.tf
+++ terraform/services/sqs.tf
@@ -47,9 +47,10 @@ resource "aws_sqs_queue_policy" "cloudtrail" {
 resource "aws_sqs_queue" "deadletter" {
   name                              = "${local.project_name}-deadletter-${local.suffix}"
   delay_seconds                     = 90
-  max_message_size                  = 2048
-  message_retention_seconds         = 604800
+  visibility_timeout_seconds        = 300
   kms_data_key_reuse_period_seconds = 300
-  receive_wait_time_seconds         = 10
+  max_message_size                  = 262144
+  message_retention_seconds         = 604800
+  receive_wait_time_seconds         = 5
 }
 
