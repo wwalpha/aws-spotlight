6966903f63a46b87dc5884d16cc46d1875a4b626
wwalpha <wwalpha@gmail.com>
2024-04-19 15:45:32 +0000

    add function

--- backend/terraform/lambda.tf
+++ backend/terraform/lambda.tf
@@ -5,7 +5,7 @@ resource "aws_lambda_function" "cloudtrail" {
   function_name = "${local.project_name}-cloudtrail-${local.suffix}"
   package_type  = "Image"
   image_uri     = data.aws_ssm_parameter.cloudtrail_repo_url.value
-  memory_size   = 512
+  memory_size   = 1024
   role          = aws_iam_role.cloudtrail.arn
   timeout       = 300
   environment {
@@ -46,6 +46,58 @@ resource "aws_lambda_function_event_invoke_config" "cloudtrail" {
 resource "aws_lambda_event_source_mapping" "cloudtrail" {
   event_source_arn                   = data.aws_sqs_queue.cloudtrail.arn
   function_name                      = aws_lambda_function.cloudtrail.arn
+  batch_size                         = 5
+  maximum_batching_window_in_seconds = 30
+  enabled                            = true
+}
+
+# ----------------------------------------------------------------------------------------------
+# Lambda Function - Filtering
+# ----------------------------------------------------------------------------------------------
+resource "aws_lambda_function" "filtering" {
+  function_name = "${local.project_name}-filtering-${local.suffix}"
+  package_type  = "Image"
+  image_uri     = data.aws_ssm_parameter.filtering_repo_url.value
+  memory_size   = 1024
+  role          = aws_iam_role.cloudtrail.arn
+  timeout       = 300
+  environment {
+    variables = {
+      TABLE_NAME_EVENT_TYPE  = local.dynamodb_name_event_type
+      TABLE_NAME_RESOURCES   = local.dynamodb_name_resources
+      TABLE_NAME_UNPROCESSED = local.dynamodb_name_unprocessed
+      TABLE_NAME_IGNORES     = local.dynamodb_name_ignores
+      SQS_URL                = data.aws_sqs_queue.filtering.url
+      SNS_TOPIC_ARN          = data.aws_sns_topic.admin.arn
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      image_uri
+    ]
+  }
+}
+
+# ----------------------------------------------------------------------------------------------
+# Lambda Function - Filtering
+# ----------------------------------------------------------------------------------------------
+resource "aws_lambda_function_event_invoke_config" "filtering" {
+  function_name = aws_lambda_function.filtering.function_name
+
+  destination_config {
+    on_failure {
+      destination = data.aws_sns_topic.admin.arn
+    }
+  }
+}
+
+# ---------------------------------------------------------------------------------------------
+# Lambda Event Source Mapping - Filtering
+# ---------------------------------------------------------------------------------------------
+resource "aws_lambda_event_source_mapping" "filtering" {
+  event_source_arn                   = data.aws_sqs_queue.filtering.arn
+  function_name                      = aws_lambda_function.filtering.arn
   batch_size                         = 5
   maximum_batching_window_in_seconds = 30
   enabled                            = true
