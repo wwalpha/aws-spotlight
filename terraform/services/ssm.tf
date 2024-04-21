1a4fca4cbd427b73280f76b4e15a5579d93660ac
wwalpha <wwalpha@gmail.com>
2024-04-21 04:08:28 +0000

    update

--- terraform/services/ssm.tf
+++ terraform/services/ssm.tf
@@ -2,10 +2,9 @@
 # SSM Parameter Store - Auth manager repository url
 # ----------------------------------------------------------------------------------------------
 resource "aws_ssm_parameter" "auth_repo_url" {
-  name      = "/${local.project_name}/repository_url/auth_manager/${local.suffix}"
-  type      = "String"
-  value     = "${aws_ecr_repository.auth.repository_url}:latest"
-  overwrite = true
+  name  = "/${local.project_name}/repository_url/auth_manager/${local.suffix}"
+  type  = "String"
+  value = "${aws_ecr_repository.auth.repository_url}:latest"
 
   lifecycle {
     ignore_changes = [
@@ -18,10 +17,9 @@ resource "aws_ssm_parameter" "auth_repo_url" {
 # SSM Parameter Store - Token manager repository url
 # ----------------------------------------------------------------------------------------------
 resource "aws_ssm_parameter" "token_repo_url" {
-  name      = "/${local.project_name}/repository_url/token_manager/${local.suffix}"
-  type      = "String"
-  value     = "${aws_ecr_repository.token.repository_url}:latest"
-  overwrite = true
+  name  = "/${local.project_name}/repository_url/token_manager/${local.suffix}"
+  type  = "String"
+  value = "${aws_ecr_repository.token.repository_url}:latest"
 
   lifecycle {
     ignore_changes = [
@@ -34,10 +32,9 @@ resource "aws_ssm_parameter" "token_repo_url" {
 # SSM Parameter Store - User manager repository url
 # ----------------------------------------------------------------------------------------------
 resource "aws_ssm_parameter" "user_repo_url" {
-  name      = "/${local.project_name}/repository_url/user_manager/${local.suffix}"
-  type      = "String"
-  value     = "${aws_ecr_repository.user.repository_url}:latest"
-  overwrite = true
+  name  = "/${local.project_name}/repository_url/user_manager/${local.suffix}"
+  type  = "String"
+  value = "${aws_ecr_repository.user.repository_url}:latest"
 
   lifecycle {
     ignore_changes = [
@@ -50,10 +47,9 @@ resource "aws_ssm_parameter" "user_repo_url" {
 # SSM Parameter Store - Resource manager repository url
 # ----------------------------------------------------------------------------------------------
 resource "aws_ssm_parameter" "resource_repo_url" {
-  name      = "/${local.project_name}/repository_url/resource_manager/${local.suffix}"
-  type      = "String"
-  value     = "${aws_ecr_repository.resource.repository_url}:latest"
-  overwrite = true
+  name  = "/${local.project_name}/repository_url/resource_manager/${local.suffix}"
+  type  = "String"
+  value = "${aws_ecr_repository.resource.repository_url}:latest"
 
   lifecycle {
     ignore_changes = [
