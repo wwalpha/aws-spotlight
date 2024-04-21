d33e8f8ef994931809b69b6b88a844016145e117
wwalpha <wwalpha@gmail.com>
2024-04-21 04:35:16 +0000

    update

--- backend/src/filtering/src/index.ts
+++ backend/src/filtering/src/index.ts
@@ -1,13 +1,12 @@
-import { SQSEvent, SQSRecord } from "aws-lambda";
-import winston from "winston";
+import { SNSMessage, SQSEvent, SQSRecord } from 'aws-lambda';
+import _ from 'lodash';
+import { CloudTrail } from 'typings';
+import { DynamodbHelper, Logger } from './utilities';
+import * as Utilities from './utilities';
 
-const Logger = winston.createLogger({
-  level: process.env.LOG_LEVEL,
-  format: winston.format.simple(),
-  transports: [new winston.transports.Console()],
-});
+const TABLE_NAME_RAW = process.env.TABLE_NAME_RAW as string;
 
-const handler = async (event: SQSEvent): Promise<APIGatewayProxyResult> => {
+export const handler = async (event: SQSEvent): Promise<void> => {
   Logger.info('event', event);
   Logger.info(`Start process records, ${event.Records.length}`);
 
@@ -31,16 +30,8 @@ const handler = async (event: SQSEvent): Promise<APIGatewayProxyResult> => {
   );
 
   // bulk insert
-  await DynamodbHelper.bulk(Environments.TABLE_NAME_RAW, dataRows);
-
-  const { queryStringParameters } = event;
-  const { filter } = queryStringParameters || {};
-  const result = await getFilteredData(filter);
-  return {
-    statusCode: 200,
-  };
-}
-
+  await DynamodbHelper.bulk(TABLE_NAME_RAW, dataRows);
+};
 
 /**
  * Process SQS Message
@@ -48,7 +39,7 @@ const handler = async (event: SQSEvent): Promise<APIGatewayProxyResult> => {
  * @param message
  */
 const executeFiltering = async (message: SQSRecord) => {
-  let records = await getRecords(message.body);
+  let records = await Utilities.getRecords(message.body);
   Logger.info(`Process All Records: ${records.length}`);
 
   // remove readonly records
@@ -59,67 +50,7 @@ const executeFiltering = async (message: SQSRecord) => {
   records = Utilities.removeError(records);
   Logger.info(`Excluding Error Records: ${records.length}`);
 
-  // remove ignore records
-  // records = Utilities.removeIgnore(records, EVENTS);
-  // Logger.info(`Excluding Ignore Records: ${records.length}`);
-
   await Utilities.deleteSQSMessage(message);
 
   return records;
 };
-
-
-/**
- * get all s3 bucket object
- *
- * @param message
- * @returns
- */
-export const getRecords = async (message: string): Promise<CloudTrail.Record[]> => {
-  const snsMessage = JSON.parse(message) as SNSMessage;
-
-  // skip validation message
-  if (snsMessage.Message.startsWith('CloudTrail validation message')) {
-    return [];
-  }
-
-  const payload = JSON.parse(snsMessage.Message) as CloudTrail.Payload;
-
-  // get files
-  const tasks = payload.s3ObjectKey.map((item) =>
-    s3Client.send(
-      new GetObjectCommand({
-        Bucket: payload.s3Bucket,
-        Key: item,
-      })
-    )
-  );
-
-  // get all files
-  const files = await Promise.all(tasks);
-
-  // unzip content
-  const records = (
-    await Promise.all(
-      files.map(async (item) => {
-        const content = await item.Body?.transformToByteArray();
-
-        if (!content) return undefined;
-
-        // @ts-ignore
-        return JSON.parse(zlib.gunzipSync(content)) as CloudTrail.Event;
-      })
-    )
-  ).filter((item): item is Exclude<typeof item, undefined> => item !== undefined);
-
-  Logger.debug('Records', records);
-
-  // merge all records
-  const newArray = records.reduce((prev, curr) => {
-    return [...prev, ...curr.Records];
-  }, [] as CloudTrail.Record[]);
-
-  // 時間順
-  return orderBy(newArray, ['eventTime'], ['asc']);
-};
-
