2a1581af1c88bab74638d9c072f6da01986bd2be
wwalpha <wwalpha@gmail.com>
2024-04-21 04:07:02 +0000

    update

--- backend/terraform/ssm.tf
+++ backend/terraform/ssm.tf
@@ -17,10 +17,9 @@ resource "aws_ssm_parameter" "cloudtrail_repo_url" {
 # SSM Parameter Store - Filtering repository url
 # ----------------------------------------------------------------------------------------------
 resource "aws_ssm_parameter" "filtering_repo_url" {
-  name      = "/${local.project_name}/repository_url/filtering"
-  type      = "String"
-  value     = "${data.aws_ecr_repository.cloudtrail.repository_url}:latest"
-  overwrite = true
+  name  = "/${local.project_name}/repository_url/filtering"
+  type  = "String"
+  value = "${data.aws_ecr_repository.cloudtrail.repository_url}:latest"
 
   lifecycle {
     ignore_changes = [
@@ -33,10 +32,9 @@ resource "aws_ssm_parameter" "filtering_repo_url" {
 # SSM Parameter Store - Unprocessed repository url
 # ----------------------------------------------------------------------------------------------
 resource "aws_ssm_parameter" "unprocessed_repo_url" {
-  name      = "/${local.project_name}/repository_url/unprocessed"
-  type      = "String"
-  value     = "${data.aws_ecr_repository.unprocessed.repository_url}:latest"
-  overwrite = true
+  name  = "/${local.project_name}/repository_url/unprocessed"
+  type  = "String"
+  value = "${data.aws_ecr_repository.unprocessed.repository_url}:latest"
 
   lifecycle {
     ignore_changes = [
