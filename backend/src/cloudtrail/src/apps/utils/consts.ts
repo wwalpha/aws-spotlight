3b0220d4012871916234be1aaac3dd67807c84ca
wwalpha <wwalpha@gmail.com>
2024-04-22 03:43:07 +0000

    update

--- backend/src/cloudtrail/src/apps/utils/consts.ts
+++ backend/src/cloudtrail/src/apps/utils/consts.ts
@@ -2,7 +2,6 @@ export const Environments = {
   TABLE_NAME_EVENT_TYPE: process.env.TABLE_NAME_EVENT_TYPE as string,
   TABLE_NAME_EVENTS: process.env.TABLE_NAME_EVENTS as string,
   TABLE_NAME_RESOURCES: process.env.TABLE_NAME_RESOURCES as string,
-  TABLE_NAME_RAW: process.env.TABLE_NAME_RAW as string,
   TABLE_NAME_UNPROCESSED: process.env.TABLE_NAME_UNPROCESSED as string,
   SNS_TOPIC_ARN: process.env.SNS_TOPIC_ARN as string,
 };
