0539653b013903a1710252011b0495b7b53a0510
wwalpha <wwalpha@gmail.com>
2024-04-23 13:11:47 +0000

    update

--- backend/src/release_events/src/utils.ts
+++ backend/src/release_events/src/utils.ts
@@ -2,6 +2,7 @@ import { DynamodbHelper } from '@alphax/dynamodb';
 import { S3, SQS, DynamoDB } from 'aws-sdk';
 import zlib from 'zlib';
 import { Tables } from 'typings';
+import _ from 'lodash';
 
 const TABLE_NAME_RESOURCES = process.env.TABLE_NAME_RESOURCES as string;
 const TABLE_NAME_HISTORY = process.env.TABLE_NAME_HISTORY as string;
@@ -17,10 +18,79 @@ export const reCreate = async () => {
   // remove all record
   await helper.truncateAll(TABLE_NAME_RESOURCES);
 
-  await reResource();
+  // await reResource();
 };
 
-export const reCreateFromBucket = async () => {};
+export const reCreateFromBucket = async () => {
+  console.log(process.env.SQS_URL);
+  console.log(process.env.BUCKET_NAME_CLOUDTRAIL);
+
+  await Promise.all([
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/us-east-1/2022/04/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/us-east-1/2022/05/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/us-east-1/2022/06/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/us-east-1/2022/07/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/us-east-1/2022/08/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/us-east-1/2022/09/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/us-east-1/2022/10/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/us-east-1/2022/11/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/us-east-1/2022/12/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/us-east-1/2023/01/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/us-east-1/2023/02/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/us-east-1/2023/03/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/us-east-1/2023/04/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/us-east-1/2023/05/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/us-east-1/2023/06/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/us-east-1/2023/07/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/us-east-1/2023/08/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/us-east-1/2023/09/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/us-east-1/2023/10/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/us-east-1/2023/11/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/us-east-1/2023/12/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/us-east-1/2024/01/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/us-east-1/2024/02/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/us-east-1/2024/03/'),
+    postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/us-east-1/2024/04/'),
+  ]);
+
+  await Promise.all([
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/ap-northeast-1/2021/06/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/ap-northeast-1/2021/07/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/ap-northeast-1/2021/08/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/ap-northeast-1/2021/09/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/ap-northeast-1/2021/10/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/ap-northeast-1/2021/11/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/ap-northeast-1/2021/12/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/ap-northeast-1/2022/01/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/ap-northeast-1/2022/02/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/ap-northeast-1/2022/03/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/ap-northeast-1/2022/04/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/ap-northeast-1/2022/05/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/ap-northeast-1/2022/06/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/ap-northeast-1/2022/07/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/ap-northeast-1/2022/08/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/ap-northeast-1/2022/09/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/ap-northeast-1/2022/10/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/ap-northeast-1/2022/11/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/ap-northeast-1/2022/12/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/ap-northeast-1/2023/01/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/ap-northeast-1/2023/02/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/ap-northeast-1/2023/03/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/ap-northeast-1/2023/04/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/ap-northeast-1/2023/05/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/ap-northeast-1/2023/06/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/ap-northeast-1/2023/07/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/ap-northeast-1/2023/08/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/ap-northeast-1/2023/09/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/ap-northeast-1/2023/10/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/ap-northeast-1/2023/11/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/ap-northeast-1/2023/12/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/ap-northeast-1/2024/01/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/ap-northeast-1/2024/02/'),
+    // postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/ap-northeast-1/2024/03/'),
+    postSQS('cloudtrail-global/AWSLogs/334678299258/CloudTrail/ap-northeast-1/2024/04/'),
+  ]);
+};
 
 const postSQS = async (s3Path: string, token?: string) => {
   const results = await s3Client
@@ -37,19 +107,20 @@ const postSQS = async (s3Path: string, token?: string) => {
     (item): item is Exclude<typeof item, undefined> => item !== undefined
   );
 
-  for (; keys.length > 0; ) {
-    const items = keys.length > 100 ? keys.splice(0, 100) : keys.splice(0, keys.length);
-
-    await sqsClient
+  const tasks = _.chunk(keys, 10).map(async (items) =>
+    sqsClient
       .sendMessage({
         QueueUrl: SQS_URL,
         MessageBody: JSON.stringify({
           Message: JSON.stringify({ s3Bucket: BUCKET_NAME_CLOUDTRAIL, s3ObjectKey: items }),
         }),
       })
-      .promise();
-    console.log('sqs send success');
-  }
+      .promise()
+  );
+
+  await Promise.all(tasks);
+
+  // await sleep(1000);
 
   console.log(results.NextContinuationToken);
 
@@ -82,44 +153,45 @@ const getBucketKeys = async (token?: string): Promise<string[]> => {
   return keys;
 };
 
-const reResource = async (lastEvaluatedKey?: DynamoDB.DocumentClient.Key) => {
-  const results = await helper.scan({
-    TableName: TABLE_NAME_HISTORY,
-    ExclusiveStartKey: lastEvaluatedKey,
-  });
+// const reResource = async (lastEvaluatedKey?: DynamoDB.DocumentClient.Key) => {
+//   const results = await helper.scan({
+//     TableName: TABLE_NAME_HISTORY,
+//     ExclusiveStartKey: lastEvaluatedKey,
+//   });
 
-  const key = `patch/${getRandom()}.json.gz`;
+//   const key = `patch/${getRandom()}.json.gz`;
 
-  // no records
-  if (!results.Items || results.Items.length === 0) {
-    return;
-  }
+//   // no records
+//   if (!results.Items || results.Items.length === 0) {
+//     return;
+//   }
 
-  const records = results.Items.map((item) => JSON.parse((item as Tables.THistory).Origin));
+//   const records = results.Items.map((item) => JSON.parse((item as Tables.THistory).Origin));
 
-  await s3Client
-    .putObject({
-      Bucket: BUCKET_NAME_ARCHIVE,
-      Key: key,
-      Body: zlib.gzipSync(JSON.stringify({ Records: records })),
-      ContentType: 'application/gz',
-    })
-    .promise();
+//   await s3Client
+//     .putObject({
+//       Bucket: BUCKET_NAME_ARCHIVE,
+//       Key: key,
+//       Body: zlib.gzipSync(JSON.stringify({ Records: records })),
+//       ContentType: 'application/gz',
+//     })
+//     .promise();
 
-  await sqsClient
-    .sendMessage({
-      QueueUrl: SQS_URL,
-      MessageBody: JSON.stringify({
-        Message: JSON.stringify({ s3Bucket: BUCKET_NAME_ARCHIVE, s3ObjectKey: [key] }),
-      }),
-    })
-    .promise();
+//   await sqsClient
+//     .sendMessage({
+//       QueueUrl: SQS_URL,
+//       MessageBody: JSON.stringify({
+//         Message: JSON.stringify({ s3Bucket: BUCKET_NAME_ARCHIVE, s3ObjectKey: [key] }),
+//       }),
+//     })
+//     .promise();
 
-  if (results.LastEvaluatedKey) {
-    await reResource(results.LastEvaluatedKey);
-  }
-};
+//   if (results.LastEvaluatedKey) {
+//     await reResource(results.LastEvaluatedKey);
+//   }
+// };
 
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 const getRandom = () => Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
 
 // reCreateFromBucket();
