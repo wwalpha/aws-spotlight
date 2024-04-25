feb2f3ea5473754e9688265215bbff8b34eb03e6
wwalpha <wwalpha@gmail.com>
2024-04-25 05:36:30 +0000

    bugfix

--- backend/terraform/lambda_layer.tf
+++ backend/terraform/lambda_layer.tf
@@ -3,8 +3,8 @@
 # ----------------------------------------------------------------------------------------------
 resource "aws_lambda_layer_version" "libraries" {
   layer_name          = "${local.project_name}-libraries-${local.suffix}"
-  s3_bucket           = data.aws_s3_object.lambda_filtering_raw.bucket
-  s3_key              = data.aws_s3_object.lambda_filtering_raw.key
-  s3_object_version   = data.aws_s3_object.lambda_filtering_raw.version_id
+  s3_bucket           = data.aws_s3_object.lambda_libraries.bucket
+  s3_key              = data.aws_s3_object.lambda_libraries.key
+  s3_object_version   = data.aws_s3_object.lambda_libraries.version_id
   compatible_runtimes = ["nodejs20.x"]
 }
