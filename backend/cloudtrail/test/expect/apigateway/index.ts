e46fcb1333e5bb4599da359b34b907066481bed5
wwalpha <wwalpha@gmail.com>
2025-04-09 08:31:02 +0000

    update

--- backend/cloudtrail/test/expect/apigateway/index.ts
+++ backend/cloudtrail/test/expect/apigateway/index.ts
@@ -1,21 +1,13 @@
-export { default as CreateRestApi_H } from './CreateRestApi_H.json';
-export { default as CreateRestApi_R } from './CreateRestApi_R.json';
-export { default as DeleteRestApi_H } from './DeleteRestApi_H.json';
-export { default as DeleteRestApi_R } from './DeleteRestApi_R.json';
-export { default as ImportRestApi_H } from './ImportRestApi_H.json';
-export { default as ImportRestApi_R } from './ImportRestApi_R.json';
-export { default as CreateApi_H } from './CreateApi_H.json';
-export { default as CreateVpcLink_H } from './CreateVpcLink_H.json';
-export { default as DeleteVpcLink_H } from './DeleteVpcLink_H.json';
+export { default as APIGATEWAY_CreateApi } from './APIGATEWAY_CreateApi.json';
+export { default as APIGATEWAY_DeleteApi } from './APIGATEWAY_DeleteApi.json';
 
-export { default as APIGATEWAY_CreateApi_R } from './APIGATEWAY_CreateApi_R.json';
-export { default as APIGATEWAY_DeleteApi_H } from './APIGATEWAY_DeleteApi_H.json';
-export { default as APIGATEWAY_DeleteApi_R } from './APIGATEWAY_DeleteApi_R.json';
+export { default as APIGATEWAY_CreateDomainName } from './APIGATEWAY_CreateDomainName.json';
+export { default as APIGATEWAY_DeleteDomainName } from './APIGATEWAY_DeleteDomainName.json';
 
-export { default as APIGATEWAY_CreateDomainName_H } from './APIGATEWAY_CreateDomainName_H.json';
-export { default as APIGATEWAY_CreateDomainName_R } from './APIGATEWAY_CreateDomainName_R.json';
-export { default as APIGATEWAY_DeleteDomainName_H } from './APIGATEWAY_DeleteDomainName_H.json';
-export { default as APIGATEWAY_DeleteDomainName_R } from './APIGATEWAY_DeleteDomainName_R.json';
+export { default as APIGATEWAY_CreateRestApi } from './APIGATEWAY_CreateRestApi.json';
+export { default as APIGATEWAY_DeleteRestApi } from './APIGATEWAY_DeleteRestApi.json';
 
-export { default as APIGATEWAY_CreateVpcLink_R } from './APIGATEWAY_CreateVpcLink_R.json';
-export { default as APIGATEWAY_DeleteVpcLink_R } from './APIGATEWAY_DeleteVpcLink_R.json';
+export { default as APIGATEWAY_CreateVpcLink } from './APIGATEWAY_CreateVpcLink.json';
+export { default as APIGATEWAY_DeleteVpcLink } from './APIGATEWAY_DeleteVpcLink.json';
+
+export { default as APIGATEWAY_ImportRestApi } from './APIGATEWAY_ImportRestApi.json';
