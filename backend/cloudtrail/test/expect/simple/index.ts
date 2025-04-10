7bf8d3caf4b610b5e1e504736d7dca529e53a250
wwalpha <wwalpha@gmail.com>
2025-04-10 00:41:16 +0000

    update

---
+++ backend/cloudtrail/test/expect/simple/index.ts
@@ -0,0 +1,14 @@
+export { default as AUTOSCALING_CreateAutoScalingGroup } from './AUTOSCALING_CreateAutoScalingGroup.json';
+export { default as AUTOSCALING_DeleteAutoScalingGroup } from './AUTOSCALING_DeleteAutoScalingGroup.json';
+
+export { default as BATCH_CreateComputeEnvironment } from './BATCH_CreateComputeEnvironment.json';
+export { default as BATCH_DeleteComputeEnvironment } from './BATCH_DeleteComputeEnvironment.json';
+
+export { default as CLOUDFRONT_CreateDistribution } from './CLOUDFRONT_CreateDistribution.json';
+export { default as CLOUDFRONT_DeleteDistribution } from './CLOUDFRONT_DeleteDistribution.json';
+
+export { default as CODEBUILD_CreateProject } from './CODEBUILD_CreateProject.json';
+export { default as CODEBUILD_DeleteProject } from './CODEBUILD_DeleteProject.json';
+
+export { default as CODEDEPLOY_CreateApplication } from './CODEDEPLOY_CreateApplication.json';
+export { default as CODEDEPLOY_DeleteApplication } from './CODEDEPLOY_DeleteApplication.json';
