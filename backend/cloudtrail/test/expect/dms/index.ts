1edc92fe2b45effa061c4ffa150ddc25397ab499
wwalpha <wwalpha@gmail.com>
2025-04-10 04:50:23 +0000

    update

--- backend/cloudtrail/test/expect/dms/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as CreateReplicationInstance_H } from './CreateReplicationInstance_H.json';
-export { default as CreateReplicationInstance_R } from './DMS_CreateReplicationInstance_R.json';
-export { default as DMS_DeleteReplicationInstance_H } from './DMS_DeleteReplicationInstance_H.json';
-export { default as DMS_DeleteReplicationInstance_R } from './DMS_DeleteReplicationInstance_R.json';
--- backend/cloudtrail/test/expect/es/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as CreateElasticsearchDomain_H } from './CreateElasticsearchDomain_H.json';
-export { default as CreateElasticsearchDomain_R } from './CreateElasticsearchDomain_R.json';
-export { default as DeleteElasticsearchDomain_H } from './DeleteElasticsearchDomain_H.json';
