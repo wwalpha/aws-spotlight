0326259b057a18271360faad58a553353b2685be
wwalpha <wwalpha@gmail.com>
2025-04-10 00:41:31 +0000

    update

---
+++ backend/cloudtrail/test/datas/index.ts
@@ -0,0 +1,203 @@
+export { default as APIGATEWAY_CreateApi } from './apigateway/APIGATEWAY_CreateApi.json';
+export { default as APIGATEWAY_CreateDomainName } from './apigateway/APIGATEWAY_CreateDomainName.json';
+export { default as APIGATEWAY_CreateRestApi } from './apigateway/APIGATEWAY_CreateRestApi.json';
+export { default as APIGATEWAY_CreateVpcLink } from './apigateway/APIGATEWAY_CreateVpcLink.json';
+export { default as APIGATEWAY_DeleteApi } from './apigateway/APIGATEWAY_DeleteApi.json';
+export { default as APIGATEWAY_DeleteDomainName } from './apigateway/APIGATEWAY_DeleteDomainName.json';
+export { default as APIGATEWAY_DeleteRestApi } from './apigateway/APIGATEWAY_DeleteRestApi.json';
+export { default as APIGATEWAY_DeleteVpcLink } from './apigateway/APIGATEWAY_DeleteVpcLink.json';
+export { default as APIGATEWAY_ImportRestApi } from './apigateway/APIGATEWAY_ImportRestApi.json';
+
+export { default as AUTOSCALING_CreateAutoScalingGroup } from './simple/AUTOSCALING_CreateAutoScalingGroup.json';
+export { default as AUTOSCALING_DeleteAutoScalingGroup } from './simple/AUTOSCALING_DeleteAutoScalingGroup.json';
+
+export { default as EC2_CreateClientVpnEndpoint } from './ec2/EC2_CreateClientVpnEndpoint.json';
+export { default as EC2_CreateImage } from './ec2/EC2_CreateImage.json';
+export { default as EC2_CreateNatGateway } from './ec2/EC2_CreateNatGateway.json';
+export { default as EC2_CreateSnapshot } from './ec2/EC2_CreateSnapshot.json';
+export { default as EC2_CreateSnapshots } from './ec2/EC2_CreateSnapshots.json';
+export { default as EC2_CopySnapshot } from './ec2/EC2_CopySnapshot.json';
+export { default as EC2_RestoreSnapshotFromRecycleBin } from './ec2/EC2_RestoreSnapshotFromRecycleBin.json';
+export { default as EC2_RunInstances } from './ec2/EC2_RunInstances.json';
+export { default as EC2_CreateVpcPeeringConnection } from './ec2/EC2_CreateVpcPeeringConnection.json';
+export { default as EC2_CreateVpc } from './ec2/EC2_CreateVpc.json';
+export { default as EC2_CreateVolume } from './ec2/EC2_CreateVolume.json';
+export { default as EC2_CreateVpcEndpoint } from './ec2/EC2_CreateVpcEndpoint.json';
+export { default as EC2_AllocateAddress } from './ec2/EC2_AllocateAddress.json';
+export { default as EC2_CreateCustomerGateway } from './ec2/EC2_CreateCustomerGateway.json';
+export { default as EC2_CreateVpnConnection } from './ec2/EC2_CreateVpnConnection.json';
+export { default as EC2_CreateVpnGateway } from './ec2/EC2_CreateVpnGateway.json';
+export { default as EC2_CreateTransitGateway } from './ec2/EC2_CreateTransitGateway.json';
+export { default as EC2_CreateSubnet } from './ec2/EC2_CreateSubnet.json';
+export { default as EC2_CreateSecurityGroup } from './ec2/EC2_CreateSecurityGroup.json';
+export { default as EC2_CreateInternetGateway } from './ec2/EC2_CreateInternetGateway.json';
+export { default as EC2_CreateNetworkInsightsPath } from './ec2/EC2_CreateNetworkInsightsPath.json';
+export { default as EC2_CreateLaunchTemplate } from './ec2/EC2_CreateLaunchTemplate.json';
+
+export { default as EC2_DeleteClientVpnEndpoint } from './ec2/EC2_DeleteClientVpnEndpoint.json';
+export { default as EC2_DeleteNatGateway } from './ec2/EC2_DeleteNatGateway.json';
+export { default as EC2_DeleteSnapshot } from './ec2/EC2_DeleteSnapshot.json';
+export { default as EC2_DeregisterImage } from './ec2/EC2_DeregisterImage.json';
+export { default as EC2_TerminateInstances } from './ec2/EC2_TerminateInstances.json';
+export { default as EC2_DeleteVpcPeeringConnection } from './ec2/EC2_DeleteVpcPeeringConnection.json';
+export { default as EC2_DeleteVpc } from './ec2/EC2_DeleteVpc.json';
+export { default as EC2_DeleteVolume } from './ec2/EC2_DeleteVolume.json';
+export { default as EC2_DeleteVpcEndpoints } from './ec2/EC2_DeleteVpcEndpoints.json';
+export { default as EC2_ReleaseAddress } from './ec2/EC2_ReleaseAddress.json';
+export { default as EC2_DeleteVpnConnection } from './ec2/EC2_DeleteVpnConnection.json';
+export { default as EC2_DeleteVpnGateway } from './ec2/EC2_DeleteVpnGateway.json';
+export { default as EC2_DeleteCustomerGateway } from './ec2/EC2_DeleteCustomerGateway.json';
+export { default as EC2_DeleteTransitGateway } from './ec2/EC2_DeleteTransitGateway.json';
+export { default as EC2_DeleteSubnet } from './ec2/EC2_DeleteSubnet.json';
+export { default as EC2_DeleteSecurityGroup } from './ec2/EC2_DeleteSecurityGroup.json';
+export { default as EC2_DeleteInternetGateway } from './ec2/EC2_DeleteInternetGateway.json';
+export { default as EC2_DeleteNetworkInsightsPath } from './ec2/EC2_DeleteNetworkInsightsPath.json';
+export { default as EC2_DeleteLaunchTemplate } from './ec2/EC2_DeleteLaunchTemplate.json';
+
+export { default as CLOUDFRONT_CreateDistribution } from './simple/CLOUDFRONT_CreateDistribution.json';
+export { default as CLOUDFRONT_DeleteDistribution } from './simple/CLOUDFRONT_DeleteDistribution.json';
+
+export { default as RDS_CreateDBCluster } from './rds/RDS_CreateDBCluster.json';
+export { default as RDS_CreateDBInstance } from './rds/RDS_CreateDBInstance.json';
+export { default as RDS_CreateDBProxy } from './rds/RDS_CreateDBProxy.json';
+export { default as RDS_CreateDBClusterParameterGroup } from './rds/RDS_CreateDBClusterParameterGroup.json';
+export { default as RDS_CreateDBParameterGroup } from './rds/RDS_CreateDBParameterGroup.json';
+export { default as RDS_CreateDBSnapshot } from './rds/RDS_CreateDBSnapshot.json';
+export { default as RDS_CopyDBSnapshot } from './rds/RDS_CopyDBSnapshot.json';
+export { default as RDS_CreateDBSubnetGroup } from './rds/RDS_CreateDBSubnetGroup.json';
+export { default as RDS_CreateDBClusterSnapshot } from './rds/RDS_CreateDBClusterSnapshot.json';
+export { default as RDS_CreateOptionGroup } from './rds/RDS_CreateOptionGroup.json';
+export { default as RDS_RestoreDBClusterFromSnapshot } from './rds/RDS_RestoreDBClusterFromSnapshot.json';
+export { default as RDS_RestoreDBClusterToPointInTime } from './rds/RDS_RestoreDBClusterToPointInTime.json';
+export { default as RDS_RestoreDBInstanceFromDBSnapshot } from './rds/RDS_RestoreDBInstanceFromDBSnapshot.json';
+export { default as RDS_RestoreDBInstanceToPointInTime } from './rds/RDS_RestoreDBInstanceToPointInTime.json';
+export { default as RDS_ModifyDBInstance } from './rds/RDS_ModifyDBInstance.json';
+export { default as RDS_CreateDBInstanceForRename } from './rds/RDS_CreateDBInstanceForRename.json';
+export { default as RDS_DeleteDBCluster } from './rds/RDS_DeleteDBCluster.json';
+export { default as RDS_DeleteDBInstance } from './rds/RDS_DeleteDBInstance.json';
+export { default as RDS_DeleteDBProxy } from './rds/RDS_DeleteDBProxy.json';
+export { default as RDS_DeleteDBClusterParameterGroup } from './rds/RDS_DeleteDBClusterParameterGroup.json';
+export { default as RDS_DeleteDBParameterGroup } from './rds/RDS_DeleteDBParameterGroup.json';
+export { default as RDS_DeleteDBSnapshot } from './rds/RDS_DeleteDBSnapshot.json';
+export { default as RDS_DeleteDBSubnetGroup } from './rds/RDS_DeleteDBSubnetGroup.json';
+export { default as RDS_DeleteOptionGroup } from './rds/RDS_DeleteOptionGroup.json';
+
+export { default as APPMESH_CreateMesh } from './simple/APPMESH_CreateMesh.json';
+export { default as APPMESH_DeleteMesh } from './simple/APPMESH_DeleteMesh.json';
+
+export { default as BACKUP_CreateBackupVault } from './backup/BACKUP_CreateBackupVault.json';
+export { default as BACKUP_CreateBackupPlan } from './backup/BACKUP_CreateBackupPlan.json';
+
+export { default as BATCH_CreateComputeEnvironment } from './simple/BATCH_CreateComputeEnvironment.json';
+export { default as BATCH_DeleteComputeEnvironment } from './simple/BATCH_DeleteComputeEnvironment.json';
+
+export { default as CODEBUILD_CreateProject } from './simple/CODEBUILD_CreateProject.json';
+export { default as CODEBUILD_DeleteProject } from './simple/CODEBUILD_DeleteProject.json';
+
+export { default as CODEDEPLOY_CreateApplication } from './simple/CODEDEPLOY_CreateApplication.json';
+export { default as CODEDEPLOY_DeleteApplication } from './simple/CODEDEPLOY_DeleteApplication.json';
+
+export { default as CONNECT_CreateInstance } from './simple/CONNECT_CreateInstance.json';
+export { default as CONNECT_DeleteInstance } from './simple/CONNECT_DeleteInstance.json';
+
+export { default as CLOUDFORMATION_CreateStack } from './simple/CLOUDFORMATION_CreateStack.json';
+export { default as CLOUDFORMATION_DeleteStack } from './simple/CLOUDFORMATION_DeleteStack.json';
+
+export { default as COGNITOIDP_CreateUserPool } from './cognito/COGNITO-IDP_CreateUserPool.json';
+export { default as COGNITOIDP_DeleteUserPool } from './cognito/COGNITO-IDP_DeleteUserPool.json';
+
+export { default as COGNITOIDENTITY_CreateIdentityPool } from './cognito/COGNITO-IDENTITY_CreateIdentityPool.json';
+export { default as COGNITOIDENTITY_DeleteIdentityPool } from './cognito/COGNITO-IDENTITY_DeleteIdentityPool.json';
+
+export { default as DS_CreateIdentityPoolDirectory } from './ds/DS_CreateIdentityPoolDirectory.json';
+export { default as DS_CreateMicrosoftAD } from './ds/DS_CreateMicrosoftAD.json';
+export { default as DS_ConnectDirectory } from './ds/DS_ConnectDirectory.json';
+export { default as DS_CreateDirectory } from './ds/DS_CreateDirectory.json';
+
+export { default as DYNAMODB_CreateTable } from './simple/DYNAMODB_CreateTable.json';
+export { default as DYNAMODB_DeleteTable } from './simple/DYNAMODB_DeleteTable.json';
+
+export { default as DMS_CreateReplicationInstance } from './simple/DMS_CreateReplicationInstance.json';
+export { default as DMS_DeleteReplicationInstance } from './simple/DMS_DeleteReplicationInstance.json';
+
+export { default as ECR_CreateRepository } from './simple/ECR_CreateRepository.json';
+export { default as ECR_DeleteRepository } from './simple/ECR_DeleteRepository.json';
+
+export { default as ECS_CreateCluster } from './simple/ECS_CreateCluster.json';
+export { default as ECS_DeleteCluster } from './simple/ECS_DeleteCluster.json';
+
+export { default as ELASTICACHE_CreateCacheCluster } from './simple/ELASTICACHE_CreateCacheCluster.json';
+export { default as ELASTICACHE_DeleteCacheCluster } from './simple/ELASTICACHE_DeleteCacheCluster.json';
+
+export { default as ELASTICLOADBALANCING_CreateLoadBalancer } from './elb/ELASTICLOADBALANCING_CreateLoadBalancer.json';
+export { default as ELASTICLOADBALANCING_DeleteLoadBalancer } from './elb/ELASTICLOADBALANCING_DeleteLoadBalancer.json';
+export { default as ELASTICLOADBALANCING_CreateTargetGroup } from './elb/ELASTICLOADBALANCING_CreateTargetGroup.json';
+export { default as ELASTICLOADBALANCING_DeleteTargetGroup } from './elb/ELASTICLOADBALANCING_DeleteTargetGroup.json';
+
+export { default as EKS_CreateCluster } from './simple/EKS_CreateCluster.json';
+export { default as EKS_DeleteCluster } from './simple/EKS_DeleteCluster.json';
+
+export { default as ELASTICFILESYSTEM_CreateFileSystem } from './simple/ELASTICFILESYSTEM_CreateFileSystem.json';
+export { default as ELASTICFILESYSTEM_DeleteFileSystem } from './simple/ELASTICFILESYSTEM_DeleteFileSystem.json';
+
+export { default as ES_CreateElasticsearchDomain } from './simple/ES_CreateElasticsearchDomain.json';
+export { default as ES_DeleteElasticsearchDomain } from './simple/ES_DeleteElasticsearchDomain.json';
+
+export { default as FIREHOSE_CreateDeliveryStream } from './simple/FIREHOSE_CreateDeliveryStream.json';
+export { default as FIREHOSE_DeleteDeliveryStream } from './simple/FIREHOSE_DeleteDeliveryStream.json';
+
+export { default as FSX_CreateFileSystem } from './simple/FSX_CreateFileSystem.json';
+export { default as FSX_DeleteFileSystem } from './simple/FSX_DeleteFileSystem.json';
+
+export { default as GLUE_CreateDatabase } from './simple/GLUE_CreateDatabase.json';
+export { default as GLUE_DeleteDatabase } from './simple/GLUE_DeleteDatabase.json';
+
+export { default as IAM_CreateAccessKey } from './iam/IAM_CreateAccessKey.json';
+export { default as IAM_CreateRole } from './iam/IAM_CreateRole.json';
+export { default as IAM_CreateSAMLProvider } from './iam/IAM_CreateSAMLProvider.json';
+export { default as IOT_CreateTopicRule } from './simple/IOT_CreateTopicRule.json';
+
+export { default as LAMBDA_CreateFunction20150331 } from './simple/LAMBDA_CreateFunction20150331.json';
+export { default as LAMBDA_DeleteFunction20150331 } from './simple/LAMBDA_DeleteFunction20150331.json';
+
+export { default as LEX_CreateBot } from './simple/LEX_CreateBot.json';
+export { default as LEX_DeleteBot } from './simple/LEX_DeleteBot.json';
+
+// export { default as KINESIS_CreateStream } from './kinesis/KINESIS_CreateStream.json';
+// export { default as KINESIS_CreateApplication } from './kinesis/KINESIS_CreateApplication.json';
+
+// export { default as MONITORING_PutMetricAlarm } from './monitoring/MONITORING_PutMetricAlarm.json';
+// export { default as MONITORING_PutDashboard } from './monitoring/MONITORING_PutDashboard.json';
+
+export { default as NFW_CreateFirewall } from './simple/NFW_CreateFirewall.json';
+export { default as NFW_DeleteFirewall } from './simple/NFW_DeleteFirewall.json';
+
+export { default as REDSHIFT_CreateCluster } from './simple/REDSHIFT_CreateCluster.json';
+export { default as REDSHIFT_DeleteCluster } from './simple/REDSHIFT_DeleteCluster.json';
+
+export { default as ROUTE53_CreateHostedZone } from './simple/ROUTE53_CreateHostedZone.json';
+export { default as ROUTE53_DeleteHostedZone } from './simple/ROUTE53_DeleteHostedZone.json';
+
+export { default as S3_CreateBucket } from './simple/S3_CreateBucket.json';
+export { default as S3_DeleteBucket } from './simple/S3_DeleteBucket.json';
+
+export { default as SNS_CreateTopic } from './simple/SNS_CreateTopic.json';
+export { default as SNS_DeleteTopic } from './simple/SNS_DeleteTopic.json';
+
+export { default as SYNTHETICS_CreateCanary } from './simple/SYNTHETICS_CreateCanary.json';
+export { default as SYNTHETICS_DeleteCanary } from './simple/SYNTHETICS_DeleteCanary.json';
+
+export { default as STATES_CreateStateMachine } from './simple/STATES_CreateStateMachine.json';
+export { default as STATES_DeleteStateMachine } from './simple/STATES_DeleteStateMachine.json';
+
+export { default as SQS_CreateQueue } from './simple/SQS_CreateQueue.json';
+export { default as SQS_DeleteQueue } from './simple/SQS_DeleteQueue.json';
+
+export { default as TRANSFER_CreateServer } from './simple/TRANSFER_CreateServer.json';
+export { default as TRANSFER_DeleteServer } from './simple/TRANSFER_DeleteServer.json';
+
+export { default as TIMESTREAM_CreateDatabase } from './simple/TIMESTREAM_CreateDatabase.json';
+export { default as TIMESTREAM_DeleteDatabase } from './simple/TIMESTREAM_DeleteDatabase.json';
+
+export { default as WAFV2_CreateIPSet } from './waf/WAFV2_CreateIPSet.json';
+export { default as WAFV2_CreateWebACL } from './waf/WAFV2_CreateWebACL.json';
