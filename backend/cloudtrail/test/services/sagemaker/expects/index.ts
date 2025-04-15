19009fab81663173c203fdfa097e3a88c7288670
wwalpha <wwalpha@gmail.com>
2025-04-15 11:25:28 +0000

    update

--- backend/cloudtrail/test/services/sagemaker/expects/index.ts
+++ backend/cloudtrail/test/services/sagemaker/expects/index.ts
@@ -1,2 +1,4 @@
 export { default as SAGEMAKER_CreateDomain } from './SAGEMAKER_CreateDomain.json';
 export { default as SAGEMAKER_DeleteDomain } from './SAGEMAKER_DeleteDomain.json';
+export { default as SAGEMAKER_CreateNotebookInstance } from './SAGEMAKER_CreateNotebookInstance.json';
+export { default as SAGEMAKER_DeleteNotebookInstance } from './SAGEMAKER_DeleteNotebookInstance.json';
--- backend/cloudtrail/test/services/sagemaker/sagemaker.test.ts
+++ backend/cloudtrail/test/services/sagemaker/sagemaker.test.ts
@@ -3,6 +3,7 @@ import { cloudtrail } from '@src/index';
 import * as Events from './datas';
 import * as EXPECTS from './expects';
 import * as fs from 'fs';
+import * as path from 'path';
 
 describe('sagemaker.amazonaws.com', () => {
   test('SAGEMAKER_CreateDomain', async () => {
@@ -22,5 +23,23 @@ describe('sagemaker.amazonaws.com', () => {
     const resource = await getResource('arn:aws:sagemaker:us-east-1:999999999999:domain/d-dml2esvpdmj6');
     expect(resource).not.toBeUndefined();
     expect(resource).toEqual(EXPECTS.SAGEMAKER_DeleteDomain);
+  });
+
+  test('SAGEMAKER_CreateNotebookInstance', async () => {
+    const event = await sendMessage(Events.SAGEMAKER_CreateNotebookInstance);
+    await cloudtrail(event);
+
+    const resource = await getResource('arn:aws:sagemaker:ap-northeast-1:999999999999:notebook-instance/mlj-notebook');
+    expect(resource).not.toBeUndefined();
+    expect(resource).toEqual(EXPECTS.SAGEMAKER_CreateNotebookInstance);
+  });
+
+  test('SAGEMAKER_DeleteNotebookInstance', async () => {
+    const event = await sendMessage(Events.SAGEMAKER_DeleteNotebookInstance);
+    await cloudtrail(event);
+
+    const resource = await getResource('arn:aws:sagemaker:ap-northeast-1:999999999999:notebook-instance/mlj-notebook');
+    expect(resource).not.toBeUndefined();
+    expect(resource).toEqual(EXPECTS.SAGEMAKER_DeleteNotebookInstance);
   });
 });
