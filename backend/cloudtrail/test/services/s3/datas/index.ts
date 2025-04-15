d3d56ba8e8b5d989d4812cb5a7f9ada726576705
wwalpha <wwalpha@gmail.com>
2025-04-15 10:36:42 +0000

    uupdate

--- backend/cloudtrail/test/services/s3/datas/index.ts
+++ backend/cloudtrail/test/services/s3/datas/index.ts
@@ -1,2 +1,4 @@
 export { default as S3_CreateBucket } from './S3_CreateBucket.json';
 export { default as S3_DeleteBucket } from './S3_DeleteBucket.json';
+export { default as S3EXPRESS_CreateBucket } from './S3EXPRESS_CreateBucket.json';
+export { default as S3EXPRESS_DeleteBucket } from './S3EXPRESS_DeleteBucket.json';
--- backend/cloudtrail/test/services/s3/s3.test.ts
+++ backend/cloudtrail/test/services/s3/s3.test.ts
@@ -3,6 +3,7 @@ import { cloudtrail } from '@src/index';
 import * as Events from './datas';
 import * as EXPECTS from './expects';
 import * as fs from 'fs';
+import * as path from 'path';
 
 describe('s3.amazonaws.com', () => {
   test('CreateBucket', async () => {
@@ -23,5 +24,28 @@ describe('s3.amazonaws.com', () => {
     const resource = await getResource('arn:aws:s3:::test-backt-testfile01');
     expect(resource).not.toBeUndefined();
     expect(resource).toEqual(EXPECTS.S3_DeleteBucket);
+  });
+});
+
+describe('s3express.amazonaws.com', () => {
+  test('S3EXPRESS_CreateBucket', async () => {
+    const event = await sendMessage(Events.S3EXPRESS_CreateBucket);
+
+    await cloudtrail(event);
+
+    const resource = await getResource('arn:aws:s3:::test-zhiyuanpan--apne1-az4--x-s3');
+    expect(resource).not.toBeUndefined();
+    expect(resource).toEqual(EXPECTS.S3EXPRESS_CreateBucket);
+  });
+
+  test('S3EXPRESS_DeleteBucket', async () => {
+    const event = await sendMessage(Events.S3EXPRESS_DeleteBucket);
+
+    await cloudtrail(event);
+
+    const resource = await getResource('arn:aws:s3:::test-zhiyuanpan--apne1-az4--x-s3');
+
+    expect(resource).not.toBeUndefined();
+    expect(resource).toEqual(EXPECTS.S3EXPRESS_DeleteBucket);
   });
 });
