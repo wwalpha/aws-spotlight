ed3e4461614880c34f35cef50176f21c3219e0a2
wwalpha <wwalpha@gmail.com>
2025-04-16 05:11:29 +0000

    update

--- terraform/eventbridge.tf
+++ terraform/eventbridge.tf
@@ -11,7 +11,7 @@ resource "aws_cloudwatch_event_rule" "athena_daily_batch" {
 # AWS CloudWatch Event Target - Athena Daily Batch
 # ----------------------------------------------------------------------------------------------
 resource "aws_cloudwatch_event_target" "athena_daily_batch" {
-  depends_on = [aws_lambda_function.daily_batch]
+  depends_on = [aws_lambda_function.daily_batch, aws_cloudwatch_event_rule.athena_daily_batch]
   rule       = aws_cloudwatch_event_rule.athena_daily_batch.name
   arn        = aws_lambda_function.daily_batch.arn
 }
