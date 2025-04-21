f183fc67949509ada47c1065204814cac5fc2119
wwalpha <wwalpha@gmail.com>
2025-04-21 00:36:21 +0000

    update

--- backend/cloudtrail/test/patch.ts
+++ backend/cloudtrail/test/patch.ts
@@ -1,10 +1,16 @@
 import { ResourceService, SettingService } from '@src/services';
+import { start as events } from './initialize/index';
 
 const start = async () => {
+  await events();
+
   await SettingService.registReportFilter({
     Id: 'REPORT_FILTERS',
     Services: {
-      iam: [''],
+      iam: ['user', 'role', 'group'],
+      ec2: ['subnet', 'volume', 'snapshot', 'network-insights-path', 'route-table', 'launch-template', 'elastic-ip'],
+      cloud9: ['environment'],
+      rds: ['pg', 'subgrp', 'og', 'cluster-pg'],
     },
   });
 
@@ -71,6 +77,58 @@ const start = async () => {
     EventSource: 'cloudformation.amazonaws.com',
     EventTime: '2022-11-30T05:21:23Z',
     Service: 'CloudFormation',
+    Status: 'Deleted',
+    UserName: 'Admin',
+  });
+
+  // arn:aws:sagemaker:ap-northeast-1:334678299258:domain/d-lvyasyudtzyf
+  await ResourceService.registLatest({
+    ResourceId: 'arn:aws:sagemaker:ap-northeast-1:334678299258:domain/d-lvyasyudtzyf',
+    AWSRegion: 'ap-northeast-1',
+    EventId: '99999999-6d19-4696-95f8-97e27ff57bad',
+    EventName: 'DeleteDomain',
+    EventSource: 'sagemaker.amazonaws.com',
+    EventTime: '2022-11-30T05:21:23Z',
+    Service: 'Sagemaker',
+    Status: 'Deleted',
+    UserName: 'Admin',
+  });
+
+  // arn:aws:rds:ap-northeast-1:334678299258:db:aurora-db-cluster-new-cluster
+  await ResourceService.registLatest({
+    ResourceId: 'arn:aws:rds:ap-northeast-1:334678299258:db:aurora-db-cluster-new-cluster',
+    AWSRegion: 'ap-northeast-1',
+    EventId: '99999999-6d19-4696-95f8-97e27ff57bad',
+    EventName: 'DeleteDBInstance',
+    EventSource: 'rds.amazonaws.com',
+    EventTime: '2022-03-30T05:21:23Z',
+    Service: 'RDS',
+    Status: 'Deleted',
+    UserName: 'Admin',
+  });
+
+  // arn:aws:rds:ap-northeast-1:334678299258:cluster:aurora-db-cluster-new-cluster
+  await ResourceService.registLatest({
+    ResourceId: 'arn:aws:rds:ap-northeast-1:334678299258:cluster:aurora-db-cluster-new-cluster',
+    AWSRegion: 'ap-northeast-1',
+    EventId: '99999999-6d19-4696-95f8-97e27ff57bad',
+    EventName: 'DeleteDBCluster',
+    EventSource: 'rds.amazonaws.com',
+    EventTime: '2022-03-30T05:21:23Z',
+    Service: 'RDS',
+    Status: 'Deleted',
+    UserName: 'Admin',
+  });
+
+  // arn:aws:rds:ap-northeast-1:334678299258:db:aurora-db-cluster
+  await ResourceService.registLatest({
+    ResourceId: 'arn:aws:rds:ap-northeast-1:334678299258:db:aurora-db-cluster',
+    AWSRegion: 'ap-northeast-1',
+    EventId: '99999999-6d19-4696-95f8-97e27ff57bad',
+    EventName: 'DeleteDBInstance',
+    EventSource: 'rds.amazonaws.com',
+    EventTime: '2022-03-30T05:21:23Z',
+    Service: 'RDS',
     Status: 'Deleted',
     UserName: 'Admin',
   });
